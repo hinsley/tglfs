b51ec6bcbff4e9189c768b1f1765867bfedc387f
Carter Hinsley <carterhinsley@outlook.com>
2024-03-24 04:06:56 +0000

    Fix dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'python-telegram-bot',
+        'telethon',
     ],
     entry_points={
         'console_scripts': [
